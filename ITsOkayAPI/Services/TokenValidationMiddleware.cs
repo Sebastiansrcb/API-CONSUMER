52381a44ef50ca5d44214e4d877834510146d7e8
Carlos Rojas <92487909+NaennuxD@users.noreply.github.com>
2024-11-02 15:30:40 -0400

    ajuste

--- ITsOkayAPI/Services/TokenValidationMiddleware.cs
+++ ITsOkayAPI/Services/TokenValidationMiddleware.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,32 +8,36 @@ using ITsOkayAPI.DataAccess;
 public class TokenValidationMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly AppDbContext _context;
 
-    public TokenValidationMiddleware(RequestDelegate next, AppDbContext context)
+    public TokenValidationMiddleware(RequestDelegate next)
     {
         _next = next;
-        _context = context;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-        if (!string.IsNullOrEmpty(token))
+        // Crear un scope para resolver el AppDbContext
+        using (var scope = context.RequestServices.CreateScope())
         {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+            var _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-            if (jwtToken != null)
+            var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (!string.IsNullOrEmpty(token))
             {
-                var username = jwtToken.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value;
-                var user = _context.Usuarios.FirstOrDefault(u => u.Nombre == username);
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
 
-                if (user != null && user.LastToken != token)
+                if (jwtToken != null)
                 {
-                    context.Response.StatusCode = 401; // Unauthorized
-                    await context.Response.WriteAsync("Token inválido o ha sido reemplazado.");
-                    return;
+                    var username = jwtToken.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value;
+                    var user = _context.Usuarios.FirstOrDefault(u => u.Nombre == username);
+
+                    if (user != null && user.LastToken != token)
+                    {
+                        context.Response.StatusCode = 401; // Unauthorized
+                        await context.Response.WriteAsync("Token inválido o ha sido reemplazado.");
+                        return;
+                    }
                 }
             }
         }
